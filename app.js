c2acc9bd323a013aec8080da6887d25dafccacab
priyanshi pal <palpriyanshi712@gmail.com>
2024-11-13 04:47:11 +0000

    explor break and continue statement

--- app.js
+++ app.js
@@ -308,4 +308,15 @@ for (let index = 0; index < newarr.length; index++) {
 }
 console.log(` length of new array is : ${newarr.length}`);
 
+/* learn break and continue sttement*/
+
+for (let i = 1; i <= 20; i++) {
+    if (i ==5){
+        console.log(`Detected 5`);
+        continue;
+    }
+
+ console.log(` Value of i is : ${i}`)
+    
+}
 
