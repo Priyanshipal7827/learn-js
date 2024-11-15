3ed2d7b49322253f2d1ab47682c4ea2243a4fc5d
priyanshi pal <palpriyanshi712@gmail.com>
2024-11-15 05:25:39 +0000

    learn event listner

--- DOM/practice.js
+++ DOM/practice.js
@@ -7,3 +7,21 @@ function addname(addnew_name){
 addname("Hitesh");
 addname("Shradha");
 addname("Aman");
+function addlanguage(newlanguage){
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(newlanguage));
+    document.querySelector(".name").appendChild(li);
+
+}
+addlanguage("java");
+addlanguage("python");
+addlanguage("golang");
+addlanguage("javascript");
+addlanguage("typescript");
+
+// remove
+
+
+    const lastlang = document.querySelector("li:last-child")
+    lastlang.remove();
+
